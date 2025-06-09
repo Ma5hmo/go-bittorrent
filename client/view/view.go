57cf8ad1c21152896111ab92681f2a8760f45355
Itay Dali <itaydali8@gmail.com>
2025-06-09 10:20:00 +0300

    aes button

--- client/view/view.go
+++ client/view/view.go
@@ -1,6 +1,7 @@
 package view
 
 import (
+	"client/common"
 	"client/view/toolbar"
 	"client/view/torrentlist"
 	"client/view/viewutils"
@@ -43,11 +44,36 @@ func CreateMainWindow() {
 	mainContent := container.NewHSplit(listsContainer, gridWithProgress)
 	mainContent.SetOffset(0.3)
 
-	// Create main container with toolbar and content
-	mainContainer := container.NewBorder(toolbar, nil, nil, nil, mainContent)
+	// Add encryption toggle button in a bottom-right toolbar
+	encryptionLabel := func() string {
+		if common.AppState.IsTrafficAESEncrypted {
+			return "AES: ON"
+		}
+		return "AES: OFF"
+	}
+	var encryptionBtn *widget.Button
+	setEncryptionBtnStyle := func() {
+		if common.AppState.IsTrafficAESEncrypted {
+			encryptionBtn.Importance = widget.HighImportance
+		} else {
+			encryptionBtn.Importance = widget.MediumImportance
+		}
+	}
+
+	encryptionBtn = widget.NewButton(encryptionLabel(), func() {
+		common.AppState.IsTrafficAESEncrypted = !common.AppState.IsTrafficAESEncrypted
+		encryptionBtn.SetText(encryptionLabel())
+		setEncryptionBtnStyle()
+	})
+	setEncryptionBtnStyle()
+
+	encryptionToolbar := container.NewHBox(encryptionBtn) // right-aligned, no label
+	bottomBar := container.NewBorder(nil, nil, nil, encryptionToolbar)
+
+	finalContainer := container.NewBorder(toolbar, bottomBar, nil, nil, mainContent)
 
 	// Set window content
-	viewutils.MainWindow.SetContent(mainContainer)
+	viewutils.MainWindow.SetContent(finalContainer)
 	viewutils.MainWindow.Show()
 	viewutils.MainApp.Run()
 }
